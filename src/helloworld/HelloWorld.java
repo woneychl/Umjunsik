f25bd37411c32e4870028edf3a032ca024d4eb99
82107 <82107@192.168.219.108>
2020-12-09 16:24:48 +0900

    1+1 = 74 추가

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,9 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 7;
+		int b = 4;
+		System.out.println("1+1 =" + a+b);
 System.out.println("Hello World");
-System.out.println("Hi World");
-System.out.println("Hello");
+
 	}
 
 }
